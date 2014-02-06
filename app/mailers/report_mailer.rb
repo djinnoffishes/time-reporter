cb771cb47f4af8a895d332f184d879666b0e017a
Mitch <wabbitsnot@gmail.com>
2014-02-06 11:34:16 -0800

    testing something

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -4,6 +4,7 @@ class ReportMailer < ActionMailer::Base
   default from: 'pmo@sleepygiant.com'
 
   def report_email
+    @tsp = CLIENT.Issue.jql("project = 'Tilting Point' AND labels = TSP")
     mail(to: 'mitch.yarchin@sleepygiant.com', subject: 'TP Time Reports - ' + Time.now.strftime("%m/%I/%y"))
   end
 
